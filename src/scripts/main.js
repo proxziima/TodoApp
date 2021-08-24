fa8b94bf317e64dc26a55fe9854c6b598ffb2433
Vinicius Queiroz <70378455+queirozvinicius@users.noreply.github.com>
2021-08-24 17:48:46 -0300

    :art: Adding media queries responsiveness
    Work with responsiveness adding mediaqueries on mobile and desktop version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
-
+  // Script para substituir os elementos ícones pelos svgs Feather
   feather.replace();
+
+  var mainContainer = document.getElementById("main-container");
+  /*--==================== SIDE MENU ====================*/
+  function openNav(){
+    
+    mainContainer.style.transform = "translateX(85vw) scaleY(.90)";
+    mainContainer.style.borderRadius = "3rem";
+    mainContainer.style.boxShadow = "-1.5rem 0 1.5rem rgba(0,0,0,0.15)";
+  }
+
+  function closeNav(){
+    mainContainer.style.transform = "";
+    mainContainer.style.borderRadius = "";
+    mainContainer.style.boxShadow = "";
+  }
   
